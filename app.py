9338e9148335d90645332cb09a2956146181dbce
LaxmanSingh01 <84785447+LaxmanSingh01@users.noreply.github.com>
2021-12-17 20:24:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import requests
+import streamlit as st
+
+API_TOKEN = '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'
+
+API_URL = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+output = query({
+    "inputs": "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.",
+})
+
+# print(output[0]['summary_text'])
+
+st.title("Text Summarization")
+
+input_sms = st.text_area("Enter the text")
+
+if st.button('Summarize'):
+    result=query(input_sms)
+    st.subheader('Your Summarized Text')
+    st.write(result[0]['summary_text'])
